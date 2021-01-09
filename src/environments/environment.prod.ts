6e9d47e62310de0c8ef8c57d7b2c9b6325f2e3ab
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2021-01-09 14:34:54 +0200

    Implemented SignIn Functionality
    Implemented SignIn Functionality using Google SignIn or Email/Password combo.
    Setup FireBase project to handle auth.
    Created AuthGuard to protect certain routes from unauthenticated users.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyANWxDblOtugKeyKshWjB0tOQTcMd5w2E0',
+    authDomain: 'kanzai-prod.firebaseapp.com',
+    projectId: 'kanzai-prod',
+    storageBucket: 'kanzai-prod.appspot.com',
+    messagingSenderId: '597538081251',
+    appId: '1:597538081251:web:4b7276fcbeb5ce2661f8d6',
+    measurementId: 'G-BPR5R8QMNQ'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyANWxDblOtugKeyKshWjB0tOQTcMd5w2E0',
+    authDomain: 'kanzai-prod.firebaseapp.com',
+    projectId: 'kanzai-prod',
+    storageBucket: 'kanzai-prod.appspot.com',
+    messagingSenderId: '597538081251',
+    appId: '1:597538081251:web:4b7276fcbeb5ce2661f8d6',
+    measurementId: 'G-BPR5R8QMNQ'
+  }
 };
 
 /*
