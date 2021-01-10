9c5713471f6f3c0e2ae2f5a7a84a5b1db0e96b98
Ignas Karpusenkovas <ignas.karpusenkovas@gmail.com>
2021-01-10 23:42:57 +0200

    Added SSR with Angular Universal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,14 @@ import {AuthGuard} from './user/auth.guard';
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
-  { path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule), canActivate: [AuthGuard] }
+  { path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule), canActivate: [AuthGuard] },
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
